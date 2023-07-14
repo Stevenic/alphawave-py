90f14c1b5bcf450c85ed5a64d689dd7bc822c68c
bdambrosio <bruce.dambrosio@gmail.com>
2023-07-14 11:02:35 -0700

    stupid bug in pythonResponseValidator

--- src/alphawave/PythonResponseValidator.py
+++ src/alphawave/PythonResponseValidator.py
@@ -8,16 +8,15 @@ import re
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
 from io import StringIO
+import subprocess
 
 class WritableObject(object):
-    "dummy output stream for pylint"
+    """"output stream for pylint"""
     def __init__(self):
         self.content = []
     def write(self, st):
-        "dummy write"
         self.content.append(st)
     def read(self):
-        "dummy read"
         return self.content
 
 class PythonResponseValidator(PromptResponseValidator):
@@ -31,21 +30,21 @@ class PythonResponseValidator(PromptResponseValidator):
         key_len = len(key)
         start = text.find(key)
         if start < 0:
-            print ('****** python extract cant find starting key')
+            #print ('****** python extract cant find starting key')
             return text, text
         text = text[start+key_len:].lstrip(' \n,')
-        print(f'****** python extract starting on {text[:10]}')
+        #print(f'****** python extract starting on {text[:10]}')
         if text.startswith('python'):
-            print(f'****** python extract stripping starting "python"')
+            #print(f'****** python extract stripping starting "python"')
             text = text[6:]
             text = text.lstrip('\n')
-            print(f'****** python extract stripped starting "python" {text[:24]}')
+            #print(f'****** python extract stripped starting "python" {text[:24]}')
         
         # look for class name
         lines = text.split('\n')
         class_name = None
         for line in lines:
-            print(f'   {line}')
+            #print(f'   {line}')
             if line.lstrip(' \n').startswith('class'):
                 class_name = (line[6:].strip()).replace(":", '')
                 if '(' in class_name:
@@ -54,52 +53,17 @@ class PythonResponseValidator(PromptResponseValidator):
                 break
         end = text.find("```")
         if end < 0:
-            print('****** python extract cant find end key')
+            #print('****** python extract cant find end key')
             return class_name, text
         return class_name, text[:end].rstrip()
 
-    """
-    def get_python(self, text):
-        # returns class name and code
-        # change - there may be imports before class
-        key = "```"
-        key_len = len(key)
-        start = text.find(key)
-        if start < 0:
-            print ('****** python extract cant find starting key')
-            return '', text
-        text = text[start+key_len:].lstrip()
-        #print(f'****** python extract starting on {text}')
-        if text.startswith('python'):
-            #print('****** python extract stripping starting "python"')
-            text = text[6:].lstrip()
-        lines = text.split('\n')
-        class_name = None
-        for line in lines:
-            #print(f'   {line}')
-            if line.lstrip().startswith('class'):
-                class_name = (line[6:].strip()).replace(":", '')
-                if '(' in class_name:
-                    class_name = class_name[:class_name.find('(')]
-                print(f'     found class name {class_name}')
-                break
-        end = text.find("```")
-        if end < 0:
-            print('****** python extract cant find end key')
-            return class_name, text
-        #text = text.replace('\\*', '*')
-        #text = text.replace('\\+', '+')
-        #text = text.replace('\\-', '-')
-        #text = text.replace('\\/', '/')
-        return class_name, text[:end].rstrip()
-    """
     
     def validate_response(self, memory: PromptMemory, functions: PromptFunctions, tokenizer: Tokenizer , response: PromptResponse, remaining_attempts: int = 0) -> Validation:
         message = response['message']
         raw_text = message if isinstance(message, str) else message.get('content', '')
         class_name, code = self.get_python(raw_text)
         if len(code) == 0:
-            print(f'***** PythonResponseValidator failure len(parsed) == 0')
+            #print(f'***** PythonResponseValidator failure len(parsed) == 0')
             return {
                 'type': 'Validation',
                 'valid': False,
@@ -108,31 +72,34 @@ class PythonResponseValidator(PromptResponseValidator):
 
         # Validate the response against pylint
         try:
-            print(f'***** PythonResponseValidator code \n{code}\n')
+            #print(f'***** PythonResponseValidator code \n{code}\n')
             with open(class_name+'.py', 'w') as cd:
                 cd.write(code)
             pylint_output = WritableObject()
-            Run(['--errors-only', class_name+'.py'], reporter=TextReporter(pylint_output), exit=False)
+            pylint_output.content=[]
+
+            pylint_out = subprocess.run(['pylint', '--errors-only', class_name+'.py'], stdout=subprocess.PIPE)
+            #Run(['--errors-only', class_name+'.py'], reporter=TextReporter(pylint_output))
             error_msgs = ''
-            for l in pylint_output.read():
+            for l in (pylint_out.stdout).decode('utf-8').split('\n'):
                 if len(l) > 4 and not l.startswith('***'):
                     error_msgs += l
             if len(error_msgs) > 0:
-                print(f'***** PythonResponseValidator code failed \n{error_msgs}\n')
+                #print(f'***** PythonResponseValidator code failed \n{error_msgs}\n')
                 return {
                     'type': 'Validation',
                     'valid': False,
                     'feedback': error_msgs
                 }
             else:
-                print(f'***** PythonResponseValidator code valid')
+                #print(f'***** PythonResponseValidator code valid')
                 return {
                     'type': 'Validation',
                     'valid': True,
                     'value': code
                 }
         except Exception as e:
-            print(f'***** PythonResponseValidator exception {str(e)}')
+            #print(f'***** PythonResponseValidator exception {str(e)}')
             return {
                 'type': 'Validation',
                 'valid': False,
---
+++ tests/agentChat.py
@@ -0,0 +1,238 @@
+import argparse, json, os, sys
+import asyncio
+import traceback
+import tkinter as tk
+import tkinter.font
+from tkinter import ttk
+from promptrix.VolatileMemory import VolatileMemory
+from promptrix.FunctionRegistry import FunctionRegistry
+from promptrix.GPT3Tokenizer import GPT3Tokenizer
+from promptrix.Prompt import Prompt
+from promptrix.SystemMessage import SystemMessage
+from promptrix.UserMessage import UserMessage
+from promptrix.AssistantMessage import AssistantMessage
+from promptrix.ConversationHistory import ConversationHistory
+from promptrix.promptrixTypes import Message, PromptFunctions, PromptMemory, Tokenizer
+from promptrix.GPT3Tokenizer import GPT3Tokenizer
+from promptrix.VolatileMemory import VolatileMemory
+from alphawave.alphawaveTypes import PromptCompletionOptions, PromptResponse, PromptResponseValidator, Validation
+from alphawave.DefaultResponseValidator import DefaultResponseValidator
+from alphawave.JSONResponseValidator import JSONResponseValidator
+from alphawave.RepairTestClient import TestClient as TestClient
+from alphawave.AlphaWave import AlphaWave
+from alphawave.OpenAIClient import OpenAIClient
+from alphawave.OSClient import OSClient
+import alphawave_pyexts.LLMClient as llm
+from alphawave_agents.Agent import Agent, AgentOptions
+from alphawave_agents.AskCommand import AskCommand
+from alphawave_agents.MathCommand import MathCommand
+from alphawave_agents.FinalAnswerCommand import FinalAnswerCommand
+from alphawave_pyexts.SearchCommand import SearchCommand
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('model', type=str, default='wizardLM', choices=llm.get_available_models().append('gpt-4'),help='prompt template model')
+args = parser.parse_args()
+if args.model is not None:
+    model = args.model
+else:
+    model = 'vicuna_v1.1'
+
+prompt_text = ''
+
+if model.startswith('gpt'):
+    client = OpenAIClient(apiKey=os.getenv('OPENAI_API_KEY'), logRequests=True)
+else:
+    #create OS client
+    client = OSClient(apiKey=None)#, logRequests=True)
+
+def make_agent():
+    global client, model, prompt_text
+    agent_options = AgentOptions(
+        client = client,
+        prompt=[prompt_text],
+        prompt_options=PromptCompletionOptions(
+            completion_type = 'chat',
+            model = model,
+            temperature = 0.1,
+            max_input_tokens = 3600,
+            max_tokens = 800,
+        ),
+        step_delay=0,
+        max_steps=30,
+        max_repair_attempts=2,
+        syntax='JSON',
+        tokenizer=GPT3Tokenizer(),
+        #logRepairs=True
+    )
+
+    # Create an agent
+    agent = Agent(options = agent_options)
+    agent.addCommand(AskCommand())
+    agent.addCommand(FinalAnswerCommand())
+    agent.addCommand(MathCommand())
+    agent.addCommand(SearchCommand(OpenAIClient(apiKey=os.getenv('OPENAI_API_KEY')), model='gpt-3.5-turbo'))
+    return agent
+
+agent = make_agent()
+
+def run_query(query):
+    global agent
+    try:
+        response = asyncio.run(agent.completeTask(query))
+        if type(response) == dict and 'type' in response and response['type'] == 'TaskResponse':
+            if response['status'] == 'success' or response['status'] == 'input_needed':
+                input_area.insert(tk.END, response['message'])
+                input_area.insert(tk.END, '\n')
+    except Exception:
+        traceback.print_exc()
+        
+def show_prompt():
+    pass
+
+def set_model():
+    pass
+
+def set(text):
+    global temperature, top_p, max_tknns
+    try:
+        if 'temp' in text:
+            idx = text.find('=')
+            if idx > 0:
+                temperature = float(text[idx+1:].strip())
+        if 'top_p' in text:
+            idx = text.find('=')
+            if idx > 0:
+                top_p = float(text[idx+1:].strip())
+        if 'max_tkns' in text:
+            idx = text.find('=')
+            if idx > 0:
+                max_tkns = int(text[idx+1:].strip())
+    except Exception as e:
+        print ("parse failed", e)
+
+PREV_LEN = 0
+def submit():
+    global PREV_LEN
+    input_text = input_area.get("1.0", tk.END)
+    if len(input_text) > PREV_LEN:
+        new_text = input_text[PREV_LEN:].strip()
+    else:
+        new_text = input_text.strip()
+    input_area.insert(tk.END, '\n\n')
+    response = run_query(new_text)
+    input_area.insert(tk.END, '\n')
+    PREV_LEN=len(input_area.get("1.0", tk.END))
+    return(input, response)
+
+def setPrompt():
+    global prompt_text, agent
+    prompt_text = input_area.get("1.0", tk.END)
+    prompt_text = prompt_text.strip()
+    if prompt_text.startswith("NoPrompt"):
+        prompt_text = ''
+    if prompt_text.startswith("Helpfull"):
+        prompt_text = 'Respond as a helpful, friendly, information assistant. Say "I don\'t know" if you have no information"'
+    if prompt_text.startswith("Bhagaven"):
+        prompt_text = 'Respond as a compassionate, empathetic, self-realized follower of Ramana Maharshi'
+    if prompt_text.startswith("ACT"):
+        prompt_text = 'Respond as a compassionate, empathetic, counselor familiar with Acceptance Commitment Theray'
+    if prompt_text.startswith("Flirt"):
+        prompt_text = 'Respond as a friendly, chatty, flirty young woman named Samantha. You are 21 years old.'
+    agent = make_agent()
+
+def clear():
+    global memory, PREV_POS
+    PREV_POS="1.0"
+    input_area.delete("1.0", tk.END)
+
+root = tk.Tk()
+
+root.title(args.model)
+root.geometry("1260x1024")
+#root.config(cursor="watch")
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+textFrame = tk.Frame(root, bg="#010101")
+controlFrame = tk.Frame(root, bg="#101010")#, width=2)
+textFrame.grid(row=0, column=0, sticky="nsew")
+controlFrame.grid(row=0, column=1, sticky="nsew")
+
+searchLevel = 0  # 0 == no search, 1 = normal search, 2 = deep search
+
+def on_value_change(*args):
+    print("The selected option is:", float(temperature.get()))
+    
+# Create a style
+style = ttk.Style()
+
+# Modify the label style
+style.configure("TLabel", background="grey", foreground="black", font=("Arial", 12))
+
+# Modify the combobox style
+style.configure("TCombobox", fieldbackground="grey", foreground="black", selectbackground="grey", font=("Arial", 12))
+# Set the font of the dropdown list
+style.configure("TCombobox", selectbackground='gray', fieldbackground='black', background='light gray')
+style.map('TCombobox', fieldbackground=[('readonly','grey')])
+style.configure("TCombobox", font=('Arial', 12))  # sets the font in the entry part
+style.configure("TCombobox.Listbox", font=('Arial', 12), background='grey')  # sets the font in the dropdown list part
+
+
+
+input_area = tk.Text(textFrame, height=80,bg='#101820', fg='AntiqueWhite1', font=("Bitstream Charter", 11), wrap=tk.WORD)
+input_area.grid(row=0, column=0, sticky='nsew')
+input_area.pack(expand=True)
+submit_button = tk.Button(controlFrame,  text="Submit", command=submit, font=("Arial", 12), bg='grey')
+submit_button.pack(side='top', fill='x')
+
+clear_button = tk.Button(controlFrame,  text="Clear", command=clear, font=("Arial",12), bg='grey')
+clear_button.pack(side='top', fill='x')
+
+temperature = tk.StringVar()
+temperature.set('.1')
+temperature.trace("w", on_value_change)
+temp_label = ttk.Label(controlFrame, text="Temperature", style='TLabel')
+temp_label.pack(side='top', fill='x')
+temp_button = ttk.Combobox(controlFrame, textvariable=temperature, values = [.01,.1,.2,.4,.5,.7,.9,1.0], style='TCombobox')
+temp_button.pack(side='top', fill='x')
+
+top_p = tk.StringVar()
+top_p.set('1.0')
+top_p_label = ttk.Label(controlFrame, text="Top_p", style='TLabel')
+top_p_label.pack(side='top', fill='x')
+top_p_button = ttk.Combobox(controlFrame, textvariable=top_p, values = [.1,.2,.3,.4,.5,.6,.7,.8,.9,1.0], style='TCombobox')
+top_p_button.pack(side='top', fill='x')
+
+max_tkns = tk.StringVar()
+max_tkns.set('250')
+max_tok_label = ttk.Label(controlFrame, text="MaxTokens", style='TLabel')
+max_tok_label.pack(side='top', fill='x')
+max_tok_button = ttk.Combobox(controlFrame, textvariable=max_tkns, values = [50,100,250,500,1000,1500,2000,4000,8000], style='TCombobox')
+max_tok_button.pack(side='top', fill='x')
+
+prompt_button = tk.Button(controlFrame, text="setPrompt", command=setPrompt, font=("Arial", 12), bg='grey')
+prompt_button.pack(side='top', fill='x')
+
+#model_button = tk.Button(controlFrame, text="gpt-3.5-turbo", command=set_model, font=("Arial", 12), bg='grey')
+#model_button.pack(side='top', fill='x')
+
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+
+root.mainloop()
+
+"""
+Find an arithmetic expression over the integers 2, 3, and 6 that evaluates to 12. You must use each integer exactly once. Use the following approach. Create a trial expression. Evaluate the results. If it is false, build hypothesis for generating a better expression, then generate a new trial expression and evaluate again. Do this until the correct result is found. If the result is 12 then the process is over.
+"""
+
+"""
+Human: you are an instruction-following AI. Follow these instructions: Find an arithmetic expression over the integers 2, 3, and 6 that evaluates to 12. You must use each integer exactly once. Use the following approach.
+Loop:
+  Generate a trial expression using previously generated hypotheses, if any
+  Evaluate the trial expression. 
+  Test if the evaluation equals 12?
+  If not, build a hypothesis to obtain higher success rate in expression generation.
+Untill Test is True.
+"""
